84892784a62e426085d6aa054787ed51cfc8dc6b
Zanark <kiodeba11@gmail.com>
2018-05-26 16:12:51 +0530

    Added Main Menu

---
+++ index.php
@@ -0,0 +1,41 @@
+<!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN">
+<html>
+<head>
+<title>Image Gallery - Main Menu</title>
+<meta http-equiv="Content-Type" content="text/html; charset=iso-8859-1">
+</head>
+<body>
+<table width="60%" border="0" align="center" cellpadding="0" cellspacing="0">
+<tr>
+<td colspan="2"><h1>Album Administration - Main Menu</h1></td>
+</tr>
+<tr>
+<td> </td>
+<td> </td>
+</tr>
+<tr>
+<td><strong>Album Functions</strong></td>
+<td><strong>Image Functions</strong></td>
+</tr>
+<tr>
+<td><a href="new_album.php">Add Album(s)</a></td>
+<td><a href="upload_images.php">Add image(s)</a></td>
+</tr>
+<tr>
+<td><a href="edit_albums.php">Edit Album(s)</a></td>
+<td><a href="edit_photos.php">Edit Photo(s)</a></td> 
+</tr>
+<tr>
+<td> </td>
+<td> </td>
+</tr>
+</table>
+<table width="60%" border="0" align="center" cellpadding="3" cellspacing="0">
+<tr>
+<br><br>
+<td><a href="index.php">Main Menu</a> | <a href="new_album.php">Create
+New Album</a> | <a href="../gallery.php">View Gallery</a></td>
+</tr>
+</table>
+</body>
+</html>

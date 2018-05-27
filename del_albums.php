57d62100ee84282b42b404e4a82c3220fdbd9319
Zanark <kiodeba11@gmail.com>
2018-05-28 01:34:42 +0530

    updtaed del_albums

--- del_albums.php
+++ del_albums.php
@@ -1,16 +1,17 @@
 <?php
- include_once("../include/config.php");
+ include_once("include/config.php");
+$msg = "";
  // No album id has been selected
  if( !$_GET['album_id'] ){
   // Display error message to user
   $msg .= "Album not selected.  Please choose an album you wish to delete!";
-  $msg .= "<br /><a href=\"edit_album.php\">Edit albums</a>";
+  $msg .= "<br /><a href=\"edit_albums.php\">Edit albums</a>";
   displayPage($msg, "Error Selecting Album");
  } else {
-  db_connect();
+  $dbcnx = db_connect();
   // Delete specified album
   $sql = "DELETE FROM albums WHERE album_id = " . addslashes($_GET['album_id']);
-  $result = @mysql_query($sql) or die("Error deleting record: " . mysql_error());
+  $result = @mysqli_query($dbcnx , $sql) or die("Error deleting record: " . mysqli_error($dbcnx));
   // Display success to user
   $msg .= "Album has been successfully deleted!<br /><a href='index.php'>Return to Admin Menu</a>";
   displayPage($msg, "Album Deleted!");

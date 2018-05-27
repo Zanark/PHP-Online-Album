c04992a038988a09c4dd5d25959c04327dd802d6
Zanark <kiodeba11@gmail.com>
2018-05-28 01:05:07 +0530

    resolved errors in add_album.php

--- add_album.php
+++ add_album.php
@@ -1,18 +1,20 @@
 <?php
       include_once("include/config.php");
+
+      $msg = "";
       // Verify that all form elements are completed
       if (empty($_POST['album_name']) || empty($_POST['album_desc'])){
    displayPage("Please complete all required fields!<br /><a href='new_album.php'>Go Back</a>", "Error Adding Album!");
          die();
       }  
       // Connect to database
-      db_connect();     
-      $sql = "INSERT INTO albums VALUES(0, '" . addslashes($_POST['album_name']) . "', '" . addslashes($_POST['album_desc']) . "', 0, '', '')";
-      $result = @mysql_query($sql) or die("Error inserting record: " . mysql_error());
+      $dbcnx = db_connect();     
+      $sql = "INSERT INTO albums VALUES(0, '" . addslashes($_POST['album_name']) . "', '" . addslashes($_POST['album_desc']) . "', 0)";
+      $result = mysqli_query($dbcnx , $sql) or die("Error inserting record: " . mysqli_error($dbcnx));
       if ($result){
-      // Notify use that album was successfully created.
+      // Notify user that album was successfully created.
    $msg .= "Album <strong>" . $_POST['album_name'] . "</strong> successfully created!";
-   $msg .= "<br /><a href='edit_album.php?album_id=" . mysql_insert_id() . "'>Click here</a> to administrate the " . $_POST['album_name'] . " album";
+   $msg .= "<br /><a href='edit_album.php?album_id=" . mysqli_insert_id($dbcnx) . "'>Click here</a> to administrate the " . $_POST['album_name'] . " album";
    $msg .= "<p><a href='index.php'>Click here</a> to return to the administrative area</p>";
    displayPage($msg, "Album " . $_POST['album_name'] . "Added!");
       }
